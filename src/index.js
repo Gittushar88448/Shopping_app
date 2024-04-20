f1103104724c5e5fb10e3db78cd4cde795607f7c
Tushar <kmrtushar01@gmail.com>
2024-04-20 16:57:44 +0530

    ading toaster

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { store } from './redux/store';
 import { Provider } from "react-redux"
+import {Toaster} from "react-hot-toast"
 import {BrowserRouter} from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,6 +13,7 @@ root.render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
+      <Toaster/>
     </Provider>
     </BrowserRouter>
 );
