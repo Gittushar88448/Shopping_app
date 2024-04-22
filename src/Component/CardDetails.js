1da95d49b23aa44b56026f918d27bc07bb9b2e12
Tushar <kmrtushar01@gmail.com>
2024-04-22 07:49:11 +0530

    adding tailwind to cardDetails component

--- src/Component/CardDetails.js
+++ src/Component/CardDetails.js
@@ -1,31 +1,60 @@
 import React from 'react'
+import {  useSelector } from 'react-redux';
 
-const CardDetails = ({post}) => {
-  return (
-    <div>
-        <div>
-            <h2>{post.title}</h2>
-        </div>
-        <div>
-            <h2>{post.description}</h2>
-        </div>
-        <div>
-            <img src={post.image}></img>
-        </div>
 
-        <div>
+const CardDetails = ({ post }) => {
+
+
+    const { ShoppingCart } = useSelector((state) => state);
+
+
+    return (
+        <div className="flex flex-col items-center justify-between 
+           hover:scale-105 transition duration-300 ease-in gap-3 p-4 mt-10 ml-5 rounded-xl border hover:shadow-2xl">
             <div>
-                <p>{post.price}</p>
+                <h2 className="text-gray-700 font-semibold text-lg text-left truncate w-40 mt-1">{post.title}</h2>
             </div>
             <div>
-                <button>
-                    ADD TO CART
-                </button>
+                <h2 className="w-40 text-gray-400 font-normal text-[10px] text-left">{post.description.split(" ").slice(0, 10).join(" ") + "..."}</h2>
             </div>
-        </div>
+            <div className="h-[180px]">
+                <img src={post.image} alt='post img' className='h-full w-full'></img>
+            </div>
 
-    </div>
-  )
+            <div  className="flex justify-between gap-12 items-center w-full mt-5">
+                <div>
+                    <p className="text-green-600 font-semibold">${post.price}</p>
+                </div>
+                <div>
+                    {
+                        ShoppingCart.some((p) => p.id === post.id) ?
+                            (
+                                <button 
+                                    className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold 
+                                        text-[12px] p-1 px-3 uppercase 
+                                        hover:bg-gray-700
+                                        hover:text-white transition duration-300 ease-in"
+                                >
+                                    REMOVE ITEM
+                                </button>
+                            )
+                            :
+                            (
+                                <button 
+                                    className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold 
+                                        text-[12px] p-1 px-3 uppercase 
+                                        hover:bg-gray-700
+                                        hover:text-white transition duration-300 ease-in"
+                                >
+                                    ADD TO CART
+                                </button>
+                            )
+                    }
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default CardDetails
