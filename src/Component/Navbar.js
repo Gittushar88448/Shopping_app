27b2f35fb76fee0f905cd2a7200baaa6f606e652
Tushar <kmrtushar01@gmail.com>
2024-04-23 21:37:39 +0530

    adding comments to nevigation bar code

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -4,10 +4,16 @@ import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+    // Fetching the ShoppingCart from the redux store
+
     const {ShoppingCart} = useSelector((state)=> state)
+
   return (
     <div className='bg-slate-900'>
         <nav className="flex justify-between items-center h-20 max-w-6xl mx-auto">
+
+            {/* When you click on image then you'll redirect to home page */}
+
             <NavLink to="/">
                 <div className='ml-5'>
                     <img src="../logo.png" className='h-14' alt='logo'></img>
@@ -15,15 +21,23 @@ const Navbar = () => {
             </NavLink>
 
             <div className='flex items-center font-medium text-slate-100 mr-5 space-x-6'>
+
+                {/* Home text do same work as above */}
+
                 <NavLink to="/">
                     <p className='hover:text-green-400 transition-all duration-200'>
                         Home
                     </p>
                 </NavLink>
+                
+                {/* Click on cart then you'll redirect to cart page */}
 
                 <NavLink to="/cart">
                     <div className='relative'>
                         {
+
+                            // Bubble which shows the currently present items in cart
+
                             ShoppingCart.length > 0 &&
                             <span 
                             className="absolute -top-1 -right-2 bg-green-600 text-xs w-5 h-5 flex 
@@ -31,6 +45,8 @@ const Navbar = () => {
                             >
                             {ShoppingCart.length}</span> 
                         }
+                        
+                        {/* Shopping icon */}
 
                         <FaShoppingCart className="text-2xl hover:text-green-400 transition-all duration-200"/>
                     </div>
